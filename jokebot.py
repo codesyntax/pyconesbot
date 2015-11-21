ce7af9d601cba6fdd6ca92fbaa99ae81b579dfd5
Anartz Mugika <amugika@codesyntax.com>
2015-11-21 20:41:18 +0100

    bot

--- jokebot.py
+++ jokebot.py
@@ -1,10 +1,22 @@
 import telebot
 import pyjokes
+import tweepy
 
-TELEBOT_TOKEN = '166480857:AAGTJBWhXvjgvjmCWtxN8ZjjgVBwwKC-atY'
+
+TELEBOT_TOKEN = os.environ.get('TELEBOT_TOKEN', '') 
+TW_CONSUMER_KEY = os.environ.get('TW_CONSUMER_KEY', '') 
+TW_CONSUMER_SECRET = os.environ.get('TW_CONSUMER_SECRET', '') 
+
 
 def start_telebot():
     tb = telebot.TeleBot(TELEBOT_TOKEN)
+    consumer_key = TW_CONSUMER_KEY
+    consumer_secret = TW_CONSUMER_SECRET
+    
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    #auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
 
     @tb.message_handler(commands=["hello",])
     def hello_handler(message):
@@ -24,7 +36,14 @@ def start_telebot():
     def dir_handler(message):
         items = ["{} -> {}".format(attr, getattr(message, attr)) for attr in dir(message)]
         tb.send_message(message.chat.id, '\n'.join(items)) 
+
+    @tb.message_handler(commands=["pycones15",])
+    def get_pycones_handler(message):
+        status_list = api.search(q="pycones15", count="10")
+        status_text = u'\n'.join([st.text for st in status_list])
+        tb.send_message(message.chat.id, status_text)
     #Use none_stop flag let polling will not stop when get new message occur error.
+    print 'Polling...'
     tb.polling()
     # Interval setup. Sleep 3 secs between request new message.
     #tb.polling(interval=3)
